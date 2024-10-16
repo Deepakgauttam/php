6c8a1fe2c0635b0f93de788d091f9d3f879be48e
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2024-10-16 18:03:37 +0530

    file valid error fix

--- filevalid.php
+++ filevalid.php
@@ -1,3 +1,24 @@
+<?php
+    // Check if the form has been submitted
+    $upload = " ";
+    if (isset($_POST['submit'])) {
+        $filename = $_FILES['doc']['name'];
+        $tempname = $_FILES['doc']['tmp_name'];
+        $filesize = $_FILES['doc']['size'];
+        $ext = end(explode('.', $_FILES['doc']['name']));
+        // $filename_parts = explode('.', $_FILES['doc']['name']);
+        // $ext = end($filename_parts);
+        $extensions = array("jpeg", "jpg", "png");
+
+        if (in_array($ext, $extensions) === true && $filesize < 2097152) {
+            move_uploaded_file($tempname, 'assets/' . $filename);
+            $upload = "file upload successfully";
+        } else {
+            $upload = "File extension should be .jpg, .jpeg, .png or File size less than 2MB";
+        }
+    }
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,34 +31,15 @@
 
 <body>
 
-    <h2>Multi File Upload Form</h2>
+    <h2>File Upload validation Form</h2>
 
     <form method="POST" enctype="multipart/form-data">
         <p><?php echo $upload; ?></p>
         <label for="file">Choose a file:</label><br>
-        <input type="file" name="doc" multiple><br><br>
+        <input type="file" name="doc"><br><br>
 
         <input type="submit" name="submit">
-    </form>
-
-    <?php
-    // Check if the form has been submitted
-    $upload = " ";
-    if (isset($_POST['submit'])) {
-        $filename = $_FILES['doc']['name'];
-        $tempname = $_FILES['doc']['tmp_name'];
-        $filesize = $_FILES['doc']['size'];
-        $fileext - end(explode('.', $_FILES['doc']['name']));
-        $extensions = array("jpeg", "jpg", "png");
-
-        if (in_array($fileext, $extensions) === true && $filesize < 2097152) {
-            move_uploaded_file($tempname, 'uploads/' . $filename);
-            $upload = "file upload successfully";
-        } else {
-            $upload = "File extension should be .jpg, .jpeg, .png or File size less than 2MB";
-        }
-    }
-    ?>
+    </form>    
 </body>
 
 </html>
